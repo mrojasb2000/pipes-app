90fc55eb9f4fe953489333bdb297e4070dc50ac0
Mauricio Rojas <mrojasb2000@gmail.com>
2024-04-09 12:30:50 -0400

    feat: adding PrimeNG modules and first button module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'pipes-app';
+export class AppComponent implements OnInit {
+
+  public title = 'pipes-app';
+
+  constructor(private primengConfig: PrimeNGConfig){}
+
+  ngOnInit(): void {
+    this.primengConfig.ripple = true;
+  }
+
 }
